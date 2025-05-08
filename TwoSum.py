91149ef6b8dcde14338cb0e4181371798641e347
Aditya <adityakatyal12345@gmail.com>
2025-05-08 19:19:38 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,26 +1,11 @@
-#Two Pointers approach
-#This approach is used to find two numbers in a sorted array that add up to a specific target number.
-#The algorithm uses two pointers, one starting at the beginning of the array and the other at the end.
-a = [3, 2, 11, 18]
-a.sort()
-target = 20
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        new = {}
 
-#Declare left and right pointers to trverse accordingly
-left = 0
-right = len(a) - 1
+        for i, num in enumerate(nums):
+            diff = target - num
 
-while left <= right:
-    b = a[left] + a[right]
-
-    if target < b:
-        right -= 1
-    elif target > b:
-        left += 1
-    else: 
-        print(f"[{left}, {right}]") 
-        break
-if left > right:
-    print("No such pair found")
-
-#Time complexity: O(nlogn) for sorting and O(n) for the two pointer approach, so overall O(nlogn)
-#Space complexity: O(1) since we are using only a constant amount of space for the pointers and variables.
+            if diff in new:
+                return [new[diff], i]
+            new[num] = i
+        return 
